dc5df994046f452f1f42ccdc87b5fb5a9de162b2
OubaidTourmine <tourmineoubaid@gmail.com>
2026-07-13 16:00:22 +0100

    commit

---
+++ lvl00/correct/polyset/set.cpp
@@ -0,0 +1,58 @@
+#include "set.hpp"
+#include "searchable_array_bag.hpp"
+
+set::set(const set &source) : bag(source.bag) {};
+
+set &set::operator=(const set &source)
+{
+    if (this != &source)
+    {
+        this->bag = source.bag;
+    }
+    return *this;
+}
+
+set::set(searchable_bag& s_bag) : bag(s_bag)
+{
+
+}
+
+bool set::has(int value) const
+{
+	return(bag.has(value));
+}
+
+void set::insert (int value)
+{
+	if(!(this->has(value)))
+		bag.insert(value);
+}
+
+void set::insert (int *data, int size)
+{
+	for(int i = 0; i < size; i++)
+	{
+		this->insert(data[i]);
+	}
+}
+
+void set::print() const
+{
+	bag.print();
+}
+
+void set::clear()
+{
+	bag.clear();
+}
+
+const searchable_bag& set::get_bag()
+{
+	return(this->bag);
+}
+
+
+set::~set()
+{
+
+}
